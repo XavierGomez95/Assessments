5c27612eb0fac0d20fe80287ff651da1fae03ddb
XavierGomez <xaviergomezxcv@gmail.com>
2025-09-26 15:34:25 +0200

    [refactor/feat]: Apply Single Responsibility Principle (SRP), restructure menu and add user-defined hierarchy levels

--- src/Persistence/EmployeeData.java
+++ src/Persistence/EmployeeData.java
@@ -7,12 +7,9 @@ import java.util.List;
 
 public class EmployeeData {
     // Método estático para obtener la lista de empleados
-    public static List<Employee> getEmployees() {
+    public static List<Employee> getEmployees(int maxLevels) {
         boolean notCeo = false;
         boolean isCeo = true, isSupervisor = true;
-        int maxLevels = 7; // Sin contar el CEO
-        // Empleados por 7 niveles> lvl_1=3, lvl_2=9 ,lvl_3=27 ,lvl_4=81 ,lvl_5=243 ,lvl_6=729 ,lvl_7=4374
-        // Calculo del número total de evaluaciones> (4374*7) + (729*6) + (243*5) + (81*4) + (27*3) + (9*2) + 3 = 36633
 
         // Nivel 0: CEO, este nivel no cuenta
         Employee ceo = new Employee(1, "Joan", isSupervisor, null, isCeo);
